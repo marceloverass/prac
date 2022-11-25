8803d23fdd50cb62a37d102638dd5f6bd2f16b2c
Marcelo Veras <marceloantonino.verass@gmail.com>
2022-11-25 00:17:49 -0300

    update

--- exJS/ex008/main.js
+++ exJS/ex008/main.js
@@ -4,8 +4,14 @@ btn.addEventListener('click', function(){
     let num = window.prompt('Digite um número:');
     res = window.document.querySelector('.resultado');
 
-    res.innerHTML = `O número a ser analisado aqui será o <strong>${num}</strong>`
-    const newHr = document.createElement("hr");
-    document.body.appendChild(newHr);
+    res.innerHTML = `O número a ser analisado aqui será o <strong>${num}</strong> <hr>
+    <p>O seu valor absoluto é ${num}</p>
+    <p>A sua parte inteira é ${parseInt(num)}</p>
+    <p>O valor inteiro mais próximo é ${Math.round(num)}</p>
+    <p>A sua raíz quadrada é ${Math.sqrt(num)}</p>
+    <p>A sua raíz cúbica é ${Math.cbrt(num)}</p>
+    <p>O valor de ${num}<sup>2</sup> é ${num**2}</p>
+    <p>O valor de ${num}<sup>3</sup> é ${num**3}</p>
+    `;
     
 })
